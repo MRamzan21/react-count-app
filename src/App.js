34d21a4231124b998d948667ed5a5b02bee1dba2
Ramzan Chaudhry <ramzan30161@gmai.com>
2023-10-14 10:42:09 -0600

    react count app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(0);
+
+  const incrementNumber = () => {
+    setCount(count + 1);
+  };
+
+  const decrementNumber = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const resetNumber = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="count-box p-3">
+        <h3 pt-2>React Count App</h3>
+        <h1 className="mt-5">{count}</h1>
+        <button className="btn btn-primary m-2 plus" onClick={incrementNumber}>
+          <i className="fa fa-plus" aria-hidden="true"></i>
+        </button>
+        <button className="btn btn-primary m-2 reset" onClick={resetNumber}>
+          <i className="fa fa-refresh" aria-hidden="true"></i>
+        </button>
+        <button className="btn btn-primary m-2 minus" onClick={decrementNumber}>
+          <i className="fa fa-minus" aria-hidden="true"></i>
+        </button>
+        
+      </div>
     </div>
   );
 }
